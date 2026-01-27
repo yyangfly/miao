512904c30b7bb5fbac972e435c68dbff63a43097
yhc999 <2211824688@qq.com>
2026-01-27 19:59:57 +0800

    lodash test

---
+++ yang_fly-lodash.js
@@ -0,0 +1,11 @@
+var yang_fly = {
+  compact: function (array) {
+    let newArr = [];
+    array.forEach(element => {
+      if (element != false && element != null && element != 0 && element != -0 && element != 0n && element !="" && element != undefined && element != NaN) {
+        newArr.push(element);
+      }
+    });
+    return newArr;
+  }
+}
